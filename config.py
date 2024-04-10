d86198bd570674bd903728129772aba80ce331a8
IDGS-802-21000388 <21000388@alumnos.utleon.edu.mx>
2024-04-09 19:09:29 -0600

    Se encripto conexion en BD

--- config.py
+++ config.py
@@ -1,10 +1,25 @@
+import base64
+
+def encrypt(cadena):
+    return base64.b64encode(cadena.encode()).decode()
+def decrypt(cadena_ofuscada):
+    return base64.b64decode(cadena_ofuscada.encode()).decode()
+
+cadena_conexion = 'mysql+pymysql://Jorge:J0rg31234.@127.0.0.1/don_galletoV2'
+
+cadena_ofuscada = encrypt(cadena_conexion)
+print("Cadena de conexión ofuscada:", cadena_ofuscada)
+
+cadena_descifrada = decrypt(cadena_ofuscada)
+print("Cadena de conexión descifrada:", cadena_descifrada)
+
 class Config(object):
     SECRET_KEY = 'CLAVE SECRETA'
     SESSION_COOKIE_SECURE=False
 
 class DevelopmentConfig(Config):
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://Jorge:J0rg31234.@127.0.0.1/don_galletoV2'
+    SQLALCHEMY_DATABASE_URI = decrypt(cadena_ofuscada)
     CORS_ORIGINS = ['http://localhost:']
     # RECAPTCHA_USE_SSL = False
     # RECAPTCHA_PUBLIC_KEY = "6Ld-ZZ0pAAAAAHAoFh6fPuhpGmck_IeSrK6SRcyR"
