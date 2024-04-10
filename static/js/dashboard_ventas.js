3a2135fc8c6c522af6cd5cab6f2a75b358b4dbcc
IDGS-802-21000455 <idgs80221000455@gmail.com>
2024-04-09 18:34:41 -0600

    Correcion Usuarios, compras y modulo ventas

---
+++ static/js/dashboard_ventas.js
@@ -0,0 +1,8 @@
+function toggleGrafica() {
+    var graficaContainer = document.getElementById("grafica-container");
+    if (graficaContainer.style.display === "none") {
+      graficaContainer.style.display = "block";
+    } else {
+      graficaContainer.style.display = "none";
+    }
+  }
